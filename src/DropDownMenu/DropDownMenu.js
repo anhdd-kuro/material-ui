anhdd-kuro/material-ui

commit bb7d54de5c718db0aadf9d2d79b150eb6132f4ab

[DropDownMenu] Expose targetOrigin and anchorOrigin properties (#6370)
* add Props anchorOrigin and targetOrigin for DropDownMenu.js

* modify Proptypes to proptypes

* modify where to that

src/DropDownMenu/DropDownMenu.js
@@ -9,11 +9,8 @@ import PopoverAnimationVertical from '../Popover/PopoverAnimationVertical';
 import keycode from 'keycode';
 import Events from '../utils/events';
 import IconButton from '../IconButton';
+import propTypes from '../utils/propTypes';
 
-const anchorOrigin = {
-  vertical: 'top',
-  horizontal: 'left',
-};
 
 function getStyles(props, context) {
   const {disabled} = props;
@@ -83,6 +80,14 @@ class DropDownMenu extends Component {
   // other user components, so it will give full access to its js styles rather
   // than just the parent.
   static propTypes = {
+    /**
+     * This is the point on the anchor that the popover's
+     * `targetOrigin` will attach to.
+     * Options:
+     * vertical: [top, center, bottom]
+     * horizontal: [left, middle, right].
+     */
+    anchorOrigin: propTypes.origin,
     /**
      * If true, the popover will apply transitions when
      * it gets added to the DOM.
@@ -181,6 +186,14 @@ class DropDownMenu extends Component {
      */
     style: PropTypes.object,
     /**
+     * This is the point on the popover which will attach to
+     * the anchor's origin.
+     * Options:
+     * vertical: [top, center, bottom]
+     * horizontal: [left, middle, right].
+     */
+    targetOrigin: propTypes.origin,
+    /**
      * Overrides the inline-styles of the underline.
      */
     underlineStyle: PropTypes.object,
@@ -200,6 +213,10 @@ class DropDownMenu extends Component {
     openImmediately: false,
     maxHeight: 500,
     multiple: false,
+    anchorOrigin: {
+      vertical: 'bottom',
+      horizontal: 'left',
+    },
   };
 
   static contextTypes = {
@@ -358,6 +375,8 @@ class DropDownMenu extends Component {
       underlineStyle,
       value,
       iconButton,
+      anchorOrigin,
+      targetOrigin,
       ...other
     } = this.props;
     const {
@@ -437,6 +456,7 @@ class DropDownMenu extends Component {
         </ClearFix>
         <Popover
           anchorOrigin={anchorOrigin}
+          targetOrigin={targetOrigin}
           anchorEl={anchorEl}
           animation={animation || PopoverAnimationVertical}
           open={open}

commit 0e53f66a3f717390910b3f8d9cc316d661398351

[DropDownMenu] Fix the breaking change
That breaking change has been introduced by
https://github.com/callemall/material-ui/pull/6370

src/DropDownMenu/DropDownMenu.js
@@ -214,7 +214,7 @@ class DropDownMenu extends Component {
     maxHeight: 500,
     multiple: false,
     anchorOrigin: {
-      vertical: 'bottom',
+      vertical: 'top',
       horizontal: 'left',
     },
   };